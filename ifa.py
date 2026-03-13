17e2342e4d8c8b5d99c3151c3421d027d1cc1540
ʜᴇʟʟᴏ, ᴍʏ ɴᴀᴍᴇ ɪꜱ <polylemma@proton.me>
2026-03-13 16:21:52 -0400

    Update ifa.py

--- ifa.py
+++ ifa.py
@@ -2,6 +2,28 @@ import random
 import time
 import os
 
+
+#from mistralai import Mistral
+
+MAJOR_ODU = [
+    "Ogbe",
+    "Oyeku",
+    "Iwori",
+    "Odi",
+    "Irosun",
+    "Owonrin",
+    "Obara",
+    "Okanran",
+    "Ogunda",
+    "Osa",
+    "Ika",
+    "Oturupon",
+    "Otura",
+    "Irete",
+    "Ose",
+    "Ofun",
+]
+
 def rand_bit():
     return random.randint(0, 1)
 
@@ -36,6 +58,16 @@ def get_bin_str_from_grid(grid):
             bits += str(grid[j][i])
     return bits
 
+def get_left_right_bits_from_grid(grid):
+    l_bits, r_bits = '', ''
+    for i in range(2):
+        for j in range(len(grid)):
+            if i == 0:
+                l_bits += str(grid[j][i])
+            if i == 1:
+                r_bits += str(grid[j][i])
+    return l_bits, r_bits
+
 def get_int_id_from_grid(grid):
     bits = get_bin_str_from_grid(grid)
 
@@ -52,20 +84,10 @@ print(get_bin_config_from_grid(grid))
 print("Binary string:")
 print(get_bin_str_from_grid(grid))
 print()
+print("Major ODU pair:")
+l_bits, r_bits = get_left_right_bits_from_grid(grid)
+l_idx, r_idx = int(l_bits, 2), int(r_bits, 2)
+print(l_bits, r_bits)
+print(f"{MAJOR_ODU[l_idx]}-{MAJOR_ODU[r_idx]}")
+print()
 print(f"ID:{get_int_id_from_grid(grid)}")
-
-"""
-|  ||
-|  |
-||  |
-|  |
-
-Binary config:
-0 0 1 0 
-1 0 0 0 
-
-Binary string:
-00101000
-
-ID:40
-"""
